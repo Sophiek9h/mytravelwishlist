328720f761f82f85ed5e680b3f58eca62bd42397
sophia <sophiek9h@gmail.com>
2024-07-09 18:58:07 +0100

    card component done

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,6 @@
 import React from "react";
-import img from "../img/benin.png"
 import imgg from "../img/Path.png"
+import ben from "../img/ben.jpg"
 
 
 
@@ -9,7 +9,7 @@ import imgg from "../img/Path.png"
 export default function Card() {
     return (
         <div className="card">
-            <img className="card-img" src={img} alt="" />
+            <img className="card-img" src={ben} alt="" />
             <div className="card-details">
                 <div className="card-location-details">
                     <img src={imgg} alt="" className="location-icon"/>
@@ -17,7 +17,7 @@ export default function Card() {
                     <a href="#" className="location-link">View on Google Maps</a>
                 </div>
                 <h1 className="place">Mount Fuji</h1>
-                <p>12 Jan, 2021 - 24 Jan, 2021</p>
+                <p className="date" >12 Jan, 2021 - 24 Jan, 2021</p>
                 <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
             </div>
         </div>
